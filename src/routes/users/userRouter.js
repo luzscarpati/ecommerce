14caac1010f635a778a78b044d1007ea064674e6
Luz Scarpati <luz.scarpati@gmail.com>
2024-03-17 16:11:00 -0300

    Creación de middleware verifyUser

--- src/routes/users/userRouter.js
+++ src/routes/users/userRouter.js
@@ -2,15 +2,16 @@ import { Router } from "express";
 import { verifyToken } from "../../middlewares/verifyToken.js";
 import UserController from "../../controllers/users/userControllers.js";
 import { checkAuthCookie } from "../../middlewares/authJwtCookies.js";
+import { verifyUser } from "../../middlewares/verifyUser.js";
 
 const controller = new UserController();
 const router = Router();
-//incorporar verifyUser
+
 router.post('/register', controller.register);
 router.post('/login', controller.login);
 router.get('/profile', verifyToken, controller.profile);
 router.get('/profile-cookie', checkAuthCookie, controller.profile);
-router.get('/all', controller.getAll);
+router.get('/all', verifyUser, controller.getAll);
 router.delete('delete', controller.delete);
 
 export default router;
