01c8c701bc535127039b914972db0e0bd42fd5eb
Luz Scarpati <luz.scarpati@gmail.com>
2024-03-18 15:08:15 -0300

    Incorporación de validador de usuario en productRouter

--- src/routes/products/productRouter.js
+++ src/routes/products/productRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import ProductController from "../../controllers/products/productControllers.js";
+import { verifyUser } from "../../middlewares/verifyUser.js";
 
 const controller = new ProductController();
 const router = Router();
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.post('/', verifyUser, controller.create);
+router.put('/:id', verifyUser, controller.update);
+router.delete('/:id', verifyUser, controller.delete);
 
 export default router;
