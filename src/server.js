d1228a4befc58afaccf33db623c592445f2c5b38
Luz Scarpati <luz.scarpati@gmail.com>
2024-03-26 11:06:39 -0300

    Instalación de cors

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import config from './config/config.js';
 import { mongoStoreOptions } from './utils/utils.js';
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { info } from './docs/info.js';
+import cors from "cors";
 
 const mainRouter = new MainRouter();
 const app = express();
