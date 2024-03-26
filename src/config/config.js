b68c7dcc75dc46f979a206ded0dca35ad5779a6f
Luz Scarpati <luz.scarpati@gmail.com>
2024-03-26 11:40:59 -0300

    Correción de configuración de documentación info

--- src/config/config.js
+++ src/config/config.js
@@ -4,12 +4,10 @@ export default {
     PORT: process.env.PORT,
     ENV: process.env.ENV,
     MONGO_URL: process.env.MONGO_URL,
-    MONGO_URL_TESTS: process.env.MONGO_URL_TESTS,
     SECRET_KEY_JWT: process.env.SECRET_KEY_JWT,
     SESSION_SECRET: process.env.SESSION_SECRET,
     PERSISTENCE: process.env.PERSISTENCE,
     SECRET_COOKIES: process.env.SECRET_COOKIES,
     EMAIL: process.env.EMAIL,
     PASSWORD: process.env.PASSWORD,
-    SECRET_COOKIES: process.env.SECRET_COOKIES
 };
